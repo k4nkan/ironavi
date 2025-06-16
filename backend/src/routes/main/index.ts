75e5c6c4feedca5a8933f895f62f284382122721
k4nkan <kanta.y.0126@gmail.com>
2025-06-17 06:34:51 +0900

    #7 バックエンドを簡潔に

--- backend/src/routes/main/index.ts
+++
@@ -1,9 +0,0 @@
-// routes/main/index.ts
-import { Router } from "express";
-import { handleMainRequest } from "./controller";
-
-const router = Router();
-
-router.post("/", handleMainRequest);
-
-export { router as mainRouter };
